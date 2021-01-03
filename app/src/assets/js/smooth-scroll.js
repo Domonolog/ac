a23c9e68d8c77a12d56c1dfde2d66663f51643f0
Stanislav <itb.frontend3@gmail.com>
2021-01-04 00:54:46 +0200

    update home and add js for html file index.html

--- app/src/assets/js/smooth-scroll.js
+++ app/src/assets/js/smooth-scroll.js
@@ -3,13 +3,4 @@ jQuery( $ => {
     $header = $( '#sticky-header' ),
     fix = 18,
     mediaBreakPoint = 992;
-
-  $( window ).on( 'load', function() {
-    setTimeout( function() {
-      if ( location.hash ) {
-        window.scrollTo( 0, 0 );
-        maybeScrollTo( location.hash );
-      }
-    }, 1 );
-  } );
 } );
